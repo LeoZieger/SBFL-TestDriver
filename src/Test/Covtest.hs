a5ad730fa58d47750f56055b5c4fccb57ad792ef
Leonard Zieger <Leonard.Zieger@outlook.de>
2023-04-12 18:48:03 +0200

    Implement testing of multiple Tests

--- src/Test/Covtest.hs
+++ src/Test/Covtest.hs
@@ -3,7 +3,7 @@ Module      : Test.Covtest
 
 A testsuite driver with support for per-test coverage information.
 -}
-module Test.Covtest ( writeInSeperateFile ) where
+module Test.Covtest ( writeInSeperateFile, Test ) where
 
 import Trace.Hpc.Tix (writeTix)
 import Trace.Hpc.Reflect (clearTix, examineTix)
@@ -11,13 +11,26 @@ import Trace.Hpc.Reflect (clearTix, examineTix)
 tixFilePrefix :: String
 tixFilePrefix = "./test/Test"
 
+-- | Test with name and result
+data Test = Test String -- ^ Name of Test
+                 (IO Bool) -- ^ Result of Test
+
+
+-- | Run multiple tests
+runTests :: [Test] -- ^ List of Tests
+         -> IO ()
+runTests ((Test name result):ts) = do p <- result
+                                      writeInSeperateFile name p
+                                      runTests ts
+
+
 -- | Run a test.
-writeInSeperateFile :: Int -- ^ The number of the test.
+writeInSeperateFile :: String -- ^ The name of the test.
                     -> Bool -- ^ The result of the test.
                     -> IO ()
-writeInSeperateFile n t = do currentTix <- examineTix
+writeInSeperateFile n p = do currentTix <- examineTix
                              writeTix (tixFilePrefix ++
                                        show n ++
-                                       (if t then "_PASS" else "_FAIL") ++
+                                       (if p then "_PASS" else "_FAIL") ++
                                        ".tix") currentTix
                              clearTix
