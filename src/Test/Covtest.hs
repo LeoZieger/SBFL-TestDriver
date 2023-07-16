fae66447cbe773a707ee26ec881b0033d711eecd
Leonard Zieger <Leonard.Zieger@outlook.de>
2023-07-16 15:28:08 +0200

    Add path parameter to running tests

--- src/Test/Covtest.hs
+++ src/Test/Covtest.hs
@@ -7,9 +7,8 @@ module Test.Covtest ( writeInSeperateFile, Test (..), runTests ) where
 
 import Trace.Hpc.Tix (writeTix)
 import Trace.Hpc.Reflect (clearTix, examineTix)
+import System.FilePath ( (<.>), (</>) )
 
-tixFilePrefix :: String
-tixFilePrefix = "./test/"
 
 -- | Test with name and result
 data Test = Test String -- ^ Name of Test
@@ -18,25 +17,25 @@ data Test = Test String -- ^ Name of Test
 
 -- | Run multiple tests
 runTests :: [Test] -- ^ List of Tests
+         -> FilePath -- ^ Folder where to place tix files
          -> IO ()
-runTests ts = do print ("Running " ++ show (length ts) ++ " Tests")
-                 go ts
-                 print "Done"
+runTests ts path = do print ("Running " ++ show (length ts) ++ " Tests")
+                      go ts
+                      print "Done"
     where
         go :: [Test] -> IO ()
         go [] = pure ()
         go ((Test name result):ts') = do print (name ++ " | " ++ show result)
-                                         writeInSeperateFile name result
+                                         writeInSeperateFile name result path
                                          go ts'
 
 
 -- | Run a test.
 writeInSeperateFile :: String -- ^ The name of the test.
                     -> Bool -- ^ The result of the test.
+                    -> FilePath -- ^ Folder where to place tix files
                     -> IO ()
-writeInSeperateFile n p = do currentTix <- examineTix
-                             writeTix (tixFilePrefix ++
-                                       n ++
-                                       (if p then "_PASS" else "_FAIL") ++
-                                       ".tix") currentTix
-                             clearTix
+writeInSeperateFile n p path = do currentTix <- examineTix
+                                  print $ "Writing to " ++ path
+                                  writeTix (path </> (n ++ (if p then "_PASS" else "_FAIL") <.> "tix")) currentTix
+                                  clearTix
